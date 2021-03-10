60274b801e90a0b9d6632b0b4be9e7b946b7c387
Андрей Серховец <serhovetc88@gmail.com>
2021-03-10 22:17:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionItemHeaders = document.querySelectorAll('.accordion__item-header');
+
+    accordionItemHeaders.forEach(accordionItemHeader => {
+        accordionItemHeader.addEventListener('click', event => {
+            accordionItemHeader.classList.toggle('active');
+            const accordionItemBody = accordionItemHeader.nextElementSibling;
+            if (accordionItemHeader.classList.contains('active')) {
+                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+            } else {
+                accordionItemBody.style.maxHeight = 0;
+            }
+        });
+    });
+});
